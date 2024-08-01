f3917c098949833d369c157109b9bad8615a6820
panpan <emily1030.pjh@gmail.com>
2024-08-01 09:03:15 +0800

    test

--- moonfall.js
+++ moonfall.js
@@ -8,7 +8,7 @@ const header = document.querySelector('.header')
 
 window.addEventListener('scroll', function() {
     let value = window.scrollY
-    stars.style.left = value * 0.25 + 'px'
+    stars.styl.left = value * 0.25 + 'px'
     moon.style.top = value * 1.05 + 'px'
     behind.style.top = value * 0.5 + 'px'
     front.style.top = value * 0 + 'px'
